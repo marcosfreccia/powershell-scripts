dd8f5a1087b7a5d91b9a81c7308c2837bb2e352e
Marcos Freccia <marcosfreccia@hotmail.com>
2018-01-17 12:23:29 +0100

    Powershell Script SyncFileSystem

---
+++ SyncFileSystem.ps1
@@ -0,0 +1,29 @@
+$SourceFolder = "D:\Source"
+$TargetFolder = "D:\Target\"
+
+$SourceFolderItems = Get-ChildItem -Recurse -Path D:\Source 
+$TargetFolderItems = Get-ChildItem  -Recurse -Path D:\Target 
+
+
+try {
+    
+    $Result = Compare-Object -ReferenceObject $SourceFolderItems -DifferenceObject $TargetFolderItems 
+
+
+    # Symbol == it means that it exists in both sides
+    # Symbol <= it means that it exists in the SourceFolder but not in the TargetFolder
+    # Symbol => it means that it exists in the Target but not in the Source
+
+    foreach ($folder in $Result) {
+        if ($folder.SideIndicator -eq "<=") {
+            $FullSourceObject = $folder.InputObject.FullName
+            $FullTargetObject = $folder.InputObject.FullName.Replace($SourceFolder, $TargetFolder)
+
+            Write-Host "Attempt to copy the following: " $FullSourceObject
+            Copy-Item -Path $FullSourceObject -Destination $FullTargetObject
+        }
+    }
+}
+catch {
+    Write-Error -Message "Something bad happened!" -ErrorAction Stop
+}
